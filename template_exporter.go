8c35d9ac7138974209bebb020b8e57aaa3e92556
Damien Plénard <damien@plenard.me>
2019-10-11 17:35:11 +0200

    add default exporter code

---
+++ template_exporter.go
@@ -0,0 +1,98 @@
+// Copyright 2019 Damien PLÉNARD.
+// Licensed under the MIT License
+
+package main
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/common/log"
+	"github.com/prometheus/common/version"
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
+)
+
+var (
+	listenAddress = kingpin.Flag("web.listen-address",
+		"Address to listen on for web interface and telemetry.").
+		Default(":9184").String()
+	metricsPath = kingpin.Flag("web.telemetry-path",
+		"Path under which to expose metrics.").
+		Default("/metrics").String()
+)
+
+const (
+	namespace = "template"
+)
+
+var (
+	up = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "", "up"),
+		"Is "+namespace+" Up.", nil, nil,
+	)
+)
+
+// Exporter collects metrics and exports them using the Prometheus metrics
+// package.
+type Exporter struct {
+}
+
+// NewExporter returns an initialized Exporter.
+func NewExporter() (*Exporter, error) {
+	return &Exporter{}, nil
+}
+
+// Describe describes all the metrics ever exported by the Twitch exporter.
+// It implements prometheus.Collector.
+func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
+	ch <- up
+}
+
+// Collect fetches data as Prometheus metrics.
+// It implements prometheus.Collector.
+func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
+	ch <- prometheus.MustNewConstMetric(
+		up, prometheus.GaugeValue, 1,
+	)
+}
+
+func init() {
+	prometheus.MustRegister(version.NewCollector(namespace + "_exporter"))
+}
+
+func main() {
+	log.AddFlags(kingpin.CommandLine)
+	kingpin.Version(version.Print(namespace + "_exporter"))
+	kingpin.HelpFlag.Short('h')
+	kingpin.Parse()
+
+	log.Infoln("Starting "+namespace+"_exporter", version.Info())
+	log.Infoln("Build context", version.BuildContext())
+
+	exporter, err := NewExporter()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	prometheus.MustRegister(exporter)
+
+	http.Handle(*metricsPath, promhttp.Handler())
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte(`<html>
+             <head><title>` + strings.Title(namespace) + ` Exporter</title></head>
+             <body>
+             <h1>` + strings.Title(namespace) + ` Exporter</h1>
+             <p><a href='` + *metricsPath + `'>Metrics</a></p>
+             <h2>Build</h2>
+             <pre>` + version.Info() + ` ` + version.BuildContext() + `</pre>
+             </body>
+             </html>`))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	log.Infoln("Listening on", *listenAddress)
+	log.Fatal(http.ListenAndServe(*listenAddress, nil))
+}
